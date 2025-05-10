92f1d3a5de609163af98fc7aa9dcb9e11e4c77a1
jailson <jailsonpacagnans@hotmail.com>
2025-05-09 22:06:21 -0300

    feat: update slideshow timing, layout and reduce sponsor image count to 28

--- js/slide.js
+++ js/slide.js
@@ -1,5 +1,5 @@
 const imagensPatrocinadores = Array.from(
-  { length: 100 },
+  { length: 28 },
   (_, i) => `imagens/patrocinador${i + 1}.png`
 );
 
@@ -31,36 +31,39 @@ function startSlideshow() {
   document.getElementById("container").style.display = "none";
   document.getElementById("start-intervalo").style.display = "none";
   document.getElementById("intervalo-container").style.display = "block";
-  
+
   // Update current images based on mode
-  currentImages = isRandomMode ? 
-    embaralhar([...imagensPatrocinadores]) : 
-    [...imagensPatrocinadores];
-  
+  currentImages = isRandomMode
+    ? embaralhar([...imagensPatrocinadores])
+    : [...imagensPatrocinadores];
+
   slideIndex = 0;
   showImage(slideIndex, currentImages);
   slideInterval = setInterval(() => {
     slideIndex = (slideIndex + 1) % currentImages.length;
     showImage(slideIndex, currentImages);
-  }, 3000);
+  }, 5000);
 }
 
 function stopSlideshow() {
   clearInterval(slideInterval);
   document.getElementById("intervalo-container").style.display = "none";
-  document.getElementById("container").style.display = "block";
+  document.getElementById("container").style.display = "flex";
   document.getElementById("start-intervalo").style.display = "block";
 }
 
 // Toggle random mode function
 function toggleRandomMode(event) {
   isRandomMode = event.target.checked;
-  document.getElementById('mode-label').textContent = 
-    isRandomMode ? 'Modo: Aleatório' : 'Modo: Sequencial';
+  document.getElementById("mode-label").textContent = isRandomMode
+    ? "Aleatório"
+    : "Sequencial";
 }
 
 document
   .getElementById("start-intervalo")
   .addEventListener("click", startSlideshow);
 document.getElementById("voltar").addEventListener("click", stopSlideshow);
-document.getElementById("random-mode-switch").addEventListener("change", toggleRandomMode);
+document
+  .getElementById("random-mode-switch")
+  .addEventListener("change", toggleRandomMode);
